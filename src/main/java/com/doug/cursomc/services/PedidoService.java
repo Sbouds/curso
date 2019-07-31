900a94f5da2a8912c9b11ee6db0019d1c402aea1
Douglas <douglasfs2812@hotmail.com>
2019-07-31 15:19:01 -0300

    Implementando toString de Pedido

--- src/main/java/com/doug/cursomc/services/PedidoService.java
+++ src/main/java/com/doug/cursomc/services/PedidoService.java
@@ -10,6 +10,7 @@ import com.doug.cursomc.domain.ItemPedido;
 import com.doug.cursomc.domain.PagamentoComBoleto;
 import com.doug.cursomc.domain.Pedido;
 import com.doug.cursomc.domain.enums.EstadoPagamento;
+import com.doug.cursomc.repositories.ClienteRepository;
 import com.doug.cursomc.repositories.ItemPedidoRepository;
 import com.doug.cursomc.repositories.PagamentoRepository;
 import com.doug.cursomc.repositories.PedidoRepository;
@@ -33,6 +34,9 @@ public class PedidoService {
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
 	
+	@Autowired
+	private ClienteService clienteService;
+	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new com.doug.cursomc.services.exceptions.ObjectNotFoundException(
@@ -42,6 +46,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -52,10 +57,12 @@ public class PedidoService {
 		pagamentoRepository.save(obj.getPagamento());
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
+		System.out.println(obj);
 		return obj;
 	}
 }
